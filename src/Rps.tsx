7421e3c53a2cd7515dbe477c413738785b422e80
raymond zhang <raymond.zhang@rpsrealsolutions.com>
2021-03-18 16:11:01 -0400

    syncfusion button

---
+++ src/Rps.tsx
@@ -0,0 +1,10 @@
+import Button from './components/Button'
+function Rps() {
+    return (
+      <div>
+          <Button></Button>
+      </div>
+    );
+  }
+  
+  export default Rps;
---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
+function Button() {
+    return (
+        <ButtonComponent>Button</ButtonComponent>
+    );
+  }
+  
+  export default Button;
