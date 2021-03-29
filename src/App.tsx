fcd16bd9b501475336b29d2c8576c525cf1e4d0c
raymond zhang <raymond.zhang@rpsrealsolutions.com>
2021-03-29 13:16:55 -0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Rps from './rps';
+import { OverView } from './components/grid/grid';
+import { SEODashboard } from './components/dashboard/analytics';
 import './App.scss';
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={Rps} />
-      <Route path="/about" component={Rps} />
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/grid">Data grid</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Rps />
+          </Route>
+          <Route path="/dashboard">
+            <SEODashboard />
+          </Route>
+          <Route path="/grid">
+            <OverView />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
