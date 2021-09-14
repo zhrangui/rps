7ff08936b0e903c7b76aeebb748e5bdef390b4a9
raymond zhang <raymond.zhang@rpsrealsolutions.com>
2021-09-14 16:44:57 -0400

    add dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom"
+import { useTranslation } from 'react-i18next';
+import { Accordion, AccordionDetails, AccordionSummary, Grid, Paper, Typography, useTheme } from '@mui/material';
+
+
+
+export default function Dashboard() {
+
+  const theme = useTheme();
+
+  const { t } = useTranslation();
+
+   return (
+    <>
+      
+    </>
+  );
+}
+
